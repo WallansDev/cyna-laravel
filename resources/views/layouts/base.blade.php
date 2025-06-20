2f06d992e8cf8112017f7417ebda681b9ea34646
BordesV <valentin.bordes@limayrac.fr>
2025-06-20 22:16:58 +0200

    correction du layout-base

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,6 +13,7 @@
     {{-- Font Awesome for icons --}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style_base.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style-card.css') }}">
 
     {{-- Additional CSS --}}
     @yield('head-content')
